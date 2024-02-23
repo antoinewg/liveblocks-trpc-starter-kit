53f66cec5e38c1d9c568e8bb6fbb966006d509ac
Antoine Garcia <a.garcia.walecha@gmail.com>
2024-02-23 09:34:28 +0100

    add context to pass session

--- pages/server/routers/liveblocks.ts
+++ pages/server/routers/liveblocks.ts
@@ -1,12 +1,9 @@
-import { Liveblocks } from "@liveblocks/node";
 import { z } from "zod";
-import { SECRET_API_KEY } from "../../../liveblocks.server.config";
+import { getDocument } from "../../../lib/server";
 import { procedure, router } from "../trpc";
 
-const liveblocks = new Liveblocks({ secret: SECRET_API_KEY as string });
-
 export const liveblocksRouter = router({
-  getRoom: procedure
-    .input(z.object({ roomId: z.string() }))
-    .query(({ input }) => liveblocks.getRoom(input.roomId)),
+  getDocument: procedure
+    .input(z.object({ documentId: z.string() }))
+    .query(({ ctx, input }) => getDocument(ctx.session, input)),
 });
